174f0029460025b011f57d6bf61a6694bef454f7
DaNaRim <rangar.danarim@gmail.com>
2022-02-09 23:04:27 +0200

    lesson 1 homework optimize

--- lesson1/homework/task2.js
+++ lesson1/homework/task2.js
@@ -1,5 +1,5 @@
 let username = prompt("Write your name");
 
-username = username.substring(0, 1).toUpperCase() + username.substring(1, username.length).toLowerCase()
+username = username.substring(0, 1).toUpperCase() + username.substring(1).toLowerCase()
 
 alert("Your name is " + username)
