9209647409c54bbac23d7bcd41ddb5e72e01480f
DaNaRim <rangar.danarim@gmail.com>
2022-02-19 01:04:32 +0200

    CORRECT lesson 2 homework: minor improvements

--- main/lesson2/homework/main.js
+++ main/lesson2/homework/main.js
@@ -3,20 +3,18 @@
 let n
 do {
   n = +prompt("Write n (integer number, from -100_000 to 99_999)")
-} while (isNaN(Number(n)) || !Number.isInteger(n)
-            || n < -100_000 || n > 99_999)
+} while (!Number.isInteger(n) || n < -100_000 || n > 99_999)
 
 let m
 do {
-  m = +prompt("Write m (integer number, from n to 100_000))")
-} while (isNaN(Number(m)) || !Number.isInteger(m)
-          || m <= n || m > 100_000)
+  m = +prompt(`Write m (integer number, from ${n} to 100_000))`)
+} while (!Number.isInteger(m) || m <= n || m > 100_000)
 
-const skipPairNum = confirm("Skip pair numbers?")
+const skipEvenNum = confirm("Skip even numbers?")
 
 let sum = 0
 for (let i = n; i <= m; i++) {
-  if (skipPairNum && i % 2 === 0) continue
+  if (skipEvenNum && i % 2 === 0) continue
   sum += i
 }
 console.log(sum)
