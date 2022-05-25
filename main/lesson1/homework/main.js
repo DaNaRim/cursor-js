8ba6f21837445c71e1448aea52bcba82a5394b44
DaNaRim <rangar.danarim@gmail.com>
2022-05-26 00:31:45 +0300

    switch case of some variables

--- main/lesson1/homework/main.js
+++ main/lesson1/homework/main.js
@@ -5,47 +5,47 @@ const PRODUCT_2 = 123.965
 const PRODUCT_3 = 90.2345
 
 //1
-const MAX = Math.max(PRODUCT_1, PRODUCT_2, PRODUCT_3)
-console.log("Maximum number: " + MAX)
+const max = Math.max(PRODUCT_1, PRODUCT_2, PRODUCT_3)
+console.log("Maximum number: " + max)
 
 //2
-const MIN = Math.min(PRODUCT_1, PRODUCT_2, PRODUCT_3)
-console.log("Minimum number: " + MIN)
+const min = Math.min(PRODUCT_1, PRODUCT_2, PRODUCT_3)
+console.log("Minimum number: " + min)
 
 //3
-const SUM = PRODUCT_1 + PRODUCT_2 + PRODUCT_3
-console.log("Amount of goods: " + SUM)
+const sum = PRODUCT_1 + PRODUCT_2 + PRODUCT_3
+console.log("Amount of goods: " + sum)
 
 //4
-const SUM_WITHOUT_PENNIES =
+const sumWithoutPennies =
   Math.floor(PRODUCT_1)
   + Math.floor(PRODUCT_2)
   + Math.floor(PRODUCT_3)
-console.log("Amount of goods without pennies: " + SUM_WITHOUT_PENNIES)
+console.log("Amount of goods without pennies: " + sumWithoutPennies)
 
 //5
-const SUM_ROUNDING_TO_HUNDREDS = Math.round(SUM / 100) * 100
-console.log("Amount of goods rounded to hundreds: " + SUM_ROUNDING_TO_HUNDREDS)
+const sumRoundingToHundreds = Math.round(sum / 100) * 100
+console.log("Amount of goods rounded to hundreds: " + sumRoundingToHundreds)
 
 //6
-const IS_SUM_EVEN = SUM_WITHOUT_PENNIES % 2 === 0
-console.log("Is sum even: " + IS_SUM_EVEN)
+const isSumEven = sumWithoutPennies % 2 === 0
+console.log("Is sum even: " + isSumEven)
 
 //7
-console.log("Change from 500: " + (500 - SUM))
+console.log("Change from 500: " + (500 - sum))
 
 //8
-const AVERAGE = +(SUM / 3).toFixed(2)
-console.log("Average value of prices: " + AVERAGE)
+const average = +(sum / 3).toFixed(2)
+console.log("Average value of prices: " + average)
 
 //9
-const RANDOM_DISCOUNT = Math.round(Math.random() * 100)
-console.log("Random discount: " + RANDOM_DISCOUNT)
+const randomDiscount = Math.round(Math.random() * 100)
+console.log("Random discount: " + randomDiscount)
 
 //10
-const SUM_WITH_DISCOUNT = +(SUM - SUM * RANDOM_DISCOUNT / 100).toFixed(2)
-console.log("Amount due: " + SUM_WITH_DISCOUNT)
+const sumWithDiscount = +(sum - sum * randomDiscount / 100).toFixed(2)
+console.log("Amount due: " + sumWithDiscount)
 
 //11
-const NET_INCOME = +(SUM / 2 - SUM * RANDOM_DISCOUNT / 100).toFixed(2)
-console.log("Net income: " + NET_INCOME)
+const netIncome = +(sum / 2 - sum * randomDiscount / 100).toFixed(2)
+console.log("Net income: " + netIncome)
