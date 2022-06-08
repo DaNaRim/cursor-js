8236ffce35be38ba648dcd955a547753b57b61d1
DaNaRim <rangar.danarim@gmail.com>
2022-06-08 19:10:08 +0300

    ADD lesson 9 homework

---
+++ main/lesson9/homework/main.js
@@ -0,0 +1,40 @@
+"use strict"
+
+const UKRAINE = {tax: 0.195, middleSalary: 1789, vacancies: 11476}
+const LATVIA = {tax: 0.25, middleSalary: 1586, vacancies: 3921}
+const LITVA = {tax: 0.15, middleSalary: 1509, vacancies: 1114}
+
+function getMyTaxes(salary) {
+  return Number((this.tax * salary).toFixed(2))
+}
+
+function getMiddleTaxes() {
+  return Number((this.tax * this.middleSalary).toFixed(2))
+}
+
+function getTotalTaxes() {
+  return Number((this.tax * this.middleSalary * this.vacancies).toFixed(2))
+}
+
+function getMySalary() {
+  const country = this
+
+  const salaryLogger = function () {
+    const salary = Math.floor(Math.random() * 500 + 1500)
+    const taxes = getMyTaxes.call(country, salary)
+    console.log({
+      salary: salary,
+      taxes: taxes,
+      profit: (salary * 100 - taxes * 100) / 100
+    })
+  }
+
+  setInterval(function () {
+    salaryLogger()
+  }, 10_000)
+}
+
+console.log(getMyTaxes.call(UKRAINE, 100))
+console.log(getMiddleTaxes.call(UKRAINE))
+console.log(getTotalTaxes.call(UKRAINE))
+getMySalary.call(UKRAINE)
